f4f73ebb7eee152be144a43a2efcf99345ce9861
HackerMukesh <43438295+HackerMukesh@users.noreply.github.com>
2019-07-11 13:50:26 +0530

    Add files via upload

---
+++ TestWebSite/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SessionIDManager manager = new SessionIDManager();
+
+        string newID = manager.CreateSessionID(Context);
+        bool redirected = false;
+        bool isAdded = false;
+        manager.SaveSessionID(Context, newID, out redirected, out isAdded);
+    }
+}
